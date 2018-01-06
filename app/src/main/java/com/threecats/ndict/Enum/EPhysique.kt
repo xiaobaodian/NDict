eba50d3f488ed64f7ec44dfc0ff82a29aab3decc
zhang <zhangji1969@163.com>
2018-01-06 23:41:53 +0800

    重新开始

---
+++ app/src/main/java/com/threecats/ndict/Enum/EPhysique.kt
@@ -0,0 +1,10 @@
+package com.threecats.ndict.Enum
+
+/**
+ * 由 zhang 创建于 2018/1/6.
+ */
+enum class EPhysique(val type: Int) {
+    poor(0),
+    normal(1),
+    strong(2)
+}
--- app/src/main/java/com/threecats/ndict/Models/Person.kt
+++ app/src/main/java/com/threecats/ndict/Models/Person.kt
@@ -1,6 +1,7 @@
 package com.threecats.ndict.Models
 
 import com.threecats.ndict.Enum.EGender
+import com.threecats.ndict.Enum.EPhysique
 import com.threecats.ndict.Enum.EWorkType
 import com.threecats.ndict.Helper.DateTime
 import io.objectbox.annotation.Convert
@@ -34,6 +35,13 @@ class Person() {
     @Convert(converter = WorkTypeConverter::class, dbType = Int::class)
     var workType: EWorkType = EWorkType.normal
 
+    @Convert(converter = PhysiqueConverter::class, dbType = Int::class)
+    var physique: EPhysique = EPhysique.normal
+
+    var RHR: Int = 60
+
+    @Transient
+    val EHR: HeartRate = HeartRate()
     @Transient
     val age: Age = Age()
     @Transient
@@ -82,6 +90,16 @@ class Person() {
         }
     }
 
+    class PhysiqueConverter: PropertyConverter<EPhysique, Int> {
+        override fun convertToEntityProperty(databaseValue: Int): EPhysique {
+            val physique = enumValues<EPhysique>()
+            return physique[databaseValue]
+        }
+        override fun convertToDatabaseValue(entityProperty: EPhysique): Int{
+            return entityProperty.type
+        }
+    }
+
     inner class Age {
         val year: Int
             get() = ((DateTime().months - birthday.months) / 12)
@@ -93,8 +111,8 @@ class Person() {
 
     inner class Bmr {
         val base: Int
-            get() = if (gender == EGender.male) (66.473+13.751*weight+5.0033*height-6.7550*age.year).toInt()
-            else (655.0955+9.463*weight+1.8496*height-4.6756*age.year).toInt()
+            get() = if (gender == EGender.male) (66.473 + 13.751 * weight + 5.0033 * height - 6.7550 * age.year).toInt()
+                    else (655.0955 + 9.463 * weight + 1.8496 * height-4.6756 * age.year).toInt()
         val normal: Int
             get() = (base * 1.4).toInt()
         val mild: Int
@@ -103,6 +121,31 @@ class Person() {
             get() = if (gender == EGender.male) (base*1.79).toInt() else (base*1.64).toInt()
         val sevete: Int
             get() = if (gender == EGender.male) (base*2.1).toInt() else (base*1.82).toInt()
+        val auto: Int
+            get() = when (workType){
+                EWorkType.normal -> normal
+                EWorkType.mild -> mild
+                EWorkType.medium -> medium
+                EWorkType.sevete -> sevete
+                else -> base
+            }
+    }
+
+    inner class HeartRate {
+        val max: Int
+            get() = 220 - age.year
+
+        fun poor(percentage: Float): Int{
+            return ((200 - age.year) * percentage).toInt()
+        }
+
+        fun normal(percentage: Float): Int{
+            return ((220 - age.year) * percentage).toInt()
+        }
+
+        fun strong(percentage: Float): Int{
+            return ((220 - age.year - RHR) * percentage + RHR).toInt()
+        }
     }
 
 }
