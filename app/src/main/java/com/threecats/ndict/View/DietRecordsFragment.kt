8ca26cc53b561340e6a7738cf8480746540aa146
zhangji <zhangji1969@163.com>
2018-01-19 16:38:55 +0800

    重新开发

--- app/src/main/java/com/threecats/ndict/View/DietRecordsFragment.kt
+++ app/src/main/java/com/threecats/ndict/View/DietRecordsFragment.kt
@@ -1,7 +1,6 @@
 package com.threecats.ndict.View
 
-import android.content.Context
-import android.net.Uri
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -11,11 +10,9 @@ import android.view.ViewGroup
 //import com.threecats.ndict.R
 import com.threecats.ndict.ndict.R
 
+
 /**
  * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [DietRecordsFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
  * Use the [DietRecordsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
@@ -24,8 +21,6 @@ class DietRecordsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var mParam1: String? = null
     private var mParam2: String? = null
-
-    private var mListener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,41 +34,6 @@ class DietRecordsFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater!!.inflate(R.layout.fragment_diet_records, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        if (mListener != null) {
-            mListener!!.onFragmentInteraction(uri)
-        }
-    }
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            mListener = context
-        } else {
-            throw RuntimeException(context!!.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
     }
 
     companion object {
@@ -100,4 +60,5 @@ class DietRecordsFragment : Fragment() {
             return fragment
         }
     }
+
 }// Required empty public constructor
---
+++ app/src/main/java/com/threecats/ndict/View/FoodsFragment.kt
@@ -0,0 +1,26 @@
+package com.threecats.ndict.View
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+//import com.threecats.ndict.R
+import com.threecats.ndict.ndict.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FoodsFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_foods, container, false)
+    }
+
+}// Required empty public constructor
