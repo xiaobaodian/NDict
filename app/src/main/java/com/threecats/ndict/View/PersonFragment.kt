7a8637e884895cb59374bbe4f1ae7e1cd4d41d73
zhangji <zhangji1969@163.com>
2018-01-22 11:33:17 +0800

    重新开发

---
+++ app/src/main/java/com/threecats/ndict/View/PersonFragment.kt
@@ -0,0 +1,24 @@
+package com.threecats.ndict.View
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.threecats.ndict.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PersonFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_person, container, false)
+    }
+
+}// Required empty public constructor
