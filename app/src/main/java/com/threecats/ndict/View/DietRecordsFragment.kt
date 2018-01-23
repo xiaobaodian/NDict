a71290223a8cbe9cdcfd73cb81b3064d1fa783d3
zhang <zhangji1969@163.com>
2018-01-23 23:09:23 +0800

    重新开始

--- app/src/main/java/com/threecats/ndict/View/DietRecordsFragment.kt
+++ app/src/main/java/com/threecats/ndict/View/DietRecordsFragment.kt
@@ -6,16 +6,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 import com.threecats.ndict.R
 import kotlinx.android.synthetic.main.fragment_diet_records.*
 
 
 class DietRecordsFragment : Fragment() {
-
-    private val mOnClickListener = View.OnClickListener { _ ->
-        date.text = "后天"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +22,12 @@ class DietRecordsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_diet_records, container, false)
-        //date.text = "后天"
-        button.setOnClickListener(mOnClickListener)
         return view
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        date.text="就这么解决了？"
+        button.setOnClickListener({ date.text="是的" })
+    }
 }
