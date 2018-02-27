8022c9d636d7efa00dc9c50f1d32741726fcd0fc
zhangji <zhangji1969@163.com>
2018-02-27 17:27:27 +0800

    重新开发

--- ndictdataset/src/main/java/com/threecats/ndictdataset/FoodFragment/FoodVitaminFragment.kt
+++ ndictdataset/src/main/java/com/threecats/ndictdataset/FoodFragment/FoodVitaminFragment.kt
@@ -6,41 +6,37 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import cn.bmob.v3.BmobQuery
 import cn.bmob.v3.datatype.BmobPointer
 import cn.bmob.v3.exception.BmobException
 import cn.bmob.v3.listener.FindListener
 import cn.bmob.v3.listener.SaveListener
-import com.threecats.ndictdataset.BDM
 import com.threecats.ndictdataset.Bmob.BFood
 import com.threecats.ndictdataset.Bmob.BFoodVitamin
 import com.threecats.ndictdataset.Enum.EditerState
-import com.threecats.ndictdataset.EventClass.UpdateFoodRecyclerItem
-
 import com.threecats.ndictdataset.R
-import org.greenrobot.eventbus.EventBus
 import org.jetbrains.anko.toast
 
 
 /**
  * A simple [Fragment] subclass.
  */
+
 class FoodVitaminFragment : FoodPropertyFragment() {
 
     var vitaminItem: BFoodVitamin? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (vitaminItem == null) {
-            findVitaminItem(currentFood)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater!!.inflate(R.layout.fragment_food_vitamin, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (vitaminItem == null) {
+            findVitaminItem(currentFood)
+        }
     }
 
     override fun getFoodFields(): Int {
@@ -61,20 +57,26 @@ class FoodVitaminFragment : FoodPropertyFragment() {
                     vitamins?.forEach { vitaminItem = it }
                 } else {
                     vitaminItem = BFoodVitamin()
-                    vitaminItem!!.Food = food
                 }
+                PreprocessingVitaminItem(vitaminItem!!)
             }
         })
     }
 
+    private fun PreprocessingVitaminItem(vitaminItem: BFoodVitamin){
+        if (vitaminItem.Food == null && editerState == EditerState.FoodEdit) {
+            saveVitaminItem(vitaminItem)
+        } // 如果上面条件成立，那就是说明vitaminItem是在编辑Food资料时没有找到对应的维生素记录而新增的
+    }
+
     private fun saveVitaminItem(vitaminItem: BFoodVitamin){
+        vitaminItem.Food = currentFood
         vitaminItem.save(object: SaveListener<String>() {
             override fun done(objectID: String?, e: BmobException?) {
                 if (e == null) {
-                    toast("添加了食材，objectID：$objectID")
-                    EventBus.getDefault().post(UpdateFoodRecyclerItem(currentFood, EditerState.FoodAppend))  //Sticky
+                    context.toast("添补了维生素数据记录")
                 } else {
-                    toast("${e.message}")
+                    context.toast("${e.message}")
                 }
             }
         })
