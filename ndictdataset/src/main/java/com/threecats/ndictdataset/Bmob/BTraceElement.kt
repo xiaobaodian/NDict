d82219c2ea0e4dae3ba24172b35c89f1fe5454c6
zhangji <zhangji1969@163.com>
2018-04-21 11:26:16 +0800

    重新调整架构

---
+++ ndictdataset/src/main/java/com/threecats/ndictdataset/Bmob/BTraceElement.kt
@@ -0,0 +1,16 @@
+package com.threecats.ndictdataset.Bmob
+
+import cn.bmob.v3.BmobObject
+import com.threecats.ndictdataset.Enum.EMeasure
+
+/**
+ * 由 zhang 于 2018/4/21 创建
+ */
+class BTraceElement: BmobObject() {
+    var nutrientID: Int = 0
+    var name: String = ""
+    var symbol: String = ""
+    var measure: EMeasure = EMeasure.Mg
+    var content: _Article? = null
+    var demand: MutableList<ProposedDosage> = ArrayList()
+}
