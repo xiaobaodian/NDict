a0a90c354e69022ddb0b1354d01f636665d26c45
zhangji <zhangji1969@163.com>
2018-04-22 14:50:27 +0800

    重新调整架构

--- ndictdataset/src/main/java/com/threecats/ndictdataset/View/ActivityNutrientEditer.kt
+++ ndictdataset/src/main/java/com/threecats/ndictdataset/View/ActivityNutrientEditer.kt
@@ -2,9 +2,12 @@ package com.threecats.ndictdataset.View
 
 import android.os.Bundle
 import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.threecats.ndictdataset.BDM
 import com.threecats.ndictdataset.NutrientFragments.NutrientDosisFragment
 import com.threecats.ndictdataset.NutrientFragments.NutrientContextFragment
+import com.threecats.ndictdataset.NutrientFragments.NutrientSublistFragment
 import com.threecats.ndictdataset.R
 import com.threecats.ndictdataset.Shells.TabViewShell.TabViewLayoutShell
 import com.threecats.ndictdataset.Shells.TabViewShell.onShellTabSelectedListener
@@ -13,13 +16,36 @@ import org.jetbrains.anko.toast
 
 class ActivityNutrientEditer : AppCompatActivity() {
 
+    private val shareSet = BDM.ShareSet!!
+
     private val viewPagerShell = TabViewLayoutShell()
+    private lateinit var workFragment: Fragment
+    private lateinit var workTitle: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nutrient_editer)
+
+        NutrientEditerToolbar.title = shareSet.currentNutrient?.name
+        NutrientEditerToolbar.setNavigationOnClickListener { onBackPressed() }
+
+        when (shareSet.currentNutrient?.nutrientID){
+            5 -> {
+                workFragment = NutrientSublistFragment()
+                workTitle = "人体所需维生素列表"
+            }
+            6 -> {
+                workFragment = NutrientSublistFragment()
+                workTitle = "人体所需微量元素列表"
+            }
+            else -> {
+                workFragment = NutrientDosisFragment()
+                workTitle = "日常需求量"
+            }
+        }
+
         viewPagerShell.setOnTabSelectedListener(object : onShellTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab, fragment: android.support.v4.app.Fragment) {
+            override fun onTabSelected(tab: TabLayout.Tab, fragment: Fragment) {
                 toast("${tab.text}")
             }
         })
@@ -27,7 +53,7 @@ class ActivityNutrientEditer : AppCompatActivity() {
                 .tab(NutrientPropertyTabs)
                 .viewPage(NutrientEditerViewPage)
                 .addFragment(NutrientContextFragment(), "描述")
-                .addFragment(NutrientDosisFragment(), "需求量")
+                .addFragment(workFragment, workTitle)
                 .link()
     }
 
