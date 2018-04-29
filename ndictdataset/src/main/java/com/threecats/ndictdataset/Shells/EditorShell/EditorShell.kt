9b3c5bfcc1f6cd6d15652a4b82bc33667eb07d67
zhangji <zhangji1969@163.com>
2018-04-29 13:37:18 +0800

    重新调整架构

---
+++ ndictdataset/src/main/java/com/threecats/ndictdataset/Shells/EditorShell/EditorShell.kt
@@ -0,0 +1,33 @@
+package com.threecats.ndictdataset.Shells.EditorShell
+
+/**
+ * 由 zhang 于 2018/4/29 创建
+ */
+
+enum class EEditState{
+    Append, Editor, Cancel
+}
+
+class EditorShell<I> {
+    var currentItem: I? = null
+    var editState: EEditState = EEditState.Append
+
+    fun append(item: I){
+        currentItem = item
+        editState = EEditState.Append
+    }
+
+    fun edit(item: I){
+        currentItem = item
+        editState = EEditState.Editor
+    }
+
+    fun cancel(){
+        editState = EEditState.Cancel
+    }
+
+    fun commit(){
+
+    }
+
+}
